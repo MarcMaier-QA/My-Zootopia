204e574df1b3ec9697ee2cc9b594774047fbaf64
MarcMaier-QA <marc_maier1990@t-online.de>
2025-08-26 15:52:03 +0200

    Improve HTML of card elements

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -14,25 +14,31 @@ animals_data = load_data('animals_data.json')
 with open("animals_template.html", "r") as handle:
     html_template = handle.read()
 
-# Generate a string with the animal data, now as HTML list items
+# Generate a string with the animal data, now with a better HTML structure
 animals_info_string = ""
 for animal in animals_data:
     # Start of the list item with the CSS class
-    animals_info_string += '<li class="cards__item">'
+    animals_info_string += '<li class="cards__item">\n'
 
-    # Add each piece of animal data with a <br/> for a line break
-    animals_info_string += f"Name: {animal.get('name')}<br/>\n"
-    animals_info_string += f"Diet: {animal.get('diet')}<br/>\n"
+    # Add the animal's name as a title
+    animals_info_string += f'  <div class="card__title">{animal.get("name")}</div>\n'
+    animals_info_string += '  <p class="card__text">\n'
 
+    # Add the diet with a bold label
+    animals_info_string += f'      <strong>Diet:</strong> {animal.get("diet")}<br/>\n'
+
+    # Check and add locations if they exist
     locations = animal.get('locations')
     if locations:
-        animals_info_string += f"Location: {locations[0]}<br/>\n"
+        animals_info_string += f'      <strong>Location:</strong> {locations[0]}<br/>\n'
 
+    # Check and add type if it exists
     animal_type = animal.get('type')
     if animal_type:
-        animals_info_string += f"Type: {animal_type}<br/>\n"
+        animals_info_string += f'      <strong>Type:</strong> {animal_type}<br/>\n'
 
-    # End the list item
+    # End the text block and the list item
+    animals_info_string += '  </p>\n'
     animals_info_string += "</li>\n"
 
 # Replace the placeholder with the generated string
@@ -42,4 +48,4 @@ final_html_content = html_template.replace("__REPLACE_ANIMALS_INFO__", animals_i
 with open("animals.html", "w") as handle:
     handle.write(final_html_content)
 
-print("animals.html has been generated successfully!")
+print("animals.html has been generated successfully with improved formatting!")
