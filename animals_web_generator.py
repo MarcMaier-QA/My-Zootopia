8c730033fbc14a7ad8526010a4346f114910c408
MarcMaier-QA <marc_maier1990@t-online.de>
2025-08-26 15:21:12 +0200

    Add code comments and fix formatting

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,21 +5,22 @@ def load_data(file_path):
     with open(file_path, "r") as handle:
         return json.load(handle)
 
-
+# Loads data from JSON file
 animals_data = load_data("animals_data.json")
 
-
+# Iterate through the list of animals and print the information
 for animal in animals_data:
     print(f"Name: {animal.get('name')}")
     print(f"Diet: {animal.get('diet')}")
 
+    # Check if 'location' exists and is not empty
+    locations = animal.get('locations')
+    if locations:
+        print(f"Locations: {locations[0]}")
 
-locations = animal.get('locations')
-if locations:
-    print(f"Locations: {locations[0]}")
+    # Check if 'type' exists
+    animal_type = animal.get('type')
+    if animal_type:
+        print(f"Type: {animal_type}")
 
-animal_type = animal.get('type')
-if animal_type:
-    print(f"Type: {animal_type}")
-
-print("-" * 20)
+print("-" * 20) # Seperator for better readability
